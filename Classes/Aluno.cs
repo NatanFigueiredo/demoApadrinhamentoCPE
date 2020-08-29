0d0ff8d08ee9a7ead0d9759243ee305a24b0689b
NatanFigueiredo <natanfigueiredo27@gmail.com>
2020-08-29 10:03:53 -0300

    Adicionar arquivos de projeto.

---
+++ Classes/Aluno.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoApadrinhamento.Classes
+{
+    class Aluno
+    {
+        string nome;
+        string turno;
+        string mbti;
+        string horário;
+
+        public Aluno(string nome, string turno, string mbti, string horário)
+        {
+            this.Nome = nome;
+            this.Turno = turno;
+            this.Mbti = mbti;
+            this.Horário = horário;
+        }
+
+        public string Nome { get => nome; set => nome = value; }
+        public string Turno { get => turno; set => turno = value; }
+        public string Mbti { get => mbti; set => mbti = value; }
+        public string Horário { get => horário; set => horário = value; }
+    }
+}
---
+++ Classes/Padrinho.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoApadrinhamento.Classes
+{
+    class Padrinho
+    {
+        public string nome { get; set; }
+        public string mbti { get; set; }
+        public string ocupacao { get; set; }
+        public string horario { get; set; }
+
+        public Padrinho(string nome, string mbti, string ocupacao, string horario)
+        {
+            this.nome = nome;
+            this.mbti = mbti;
+            this.ocupacao = ocupacao;
+            this.horario = horario;
+        }
+    }
+}
